bd9ef5ec8262ca171aab535d5bad8a27d86506d2
13octt <88072761+13octt@users.noreply.github.com>
2023-08-26 10:00:43 +0700

    Update search screen and card ui

--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -1,60 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:music_app/components/search_bar.dart';
 
-class SearchScreen extends StatefulWidget {
+class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
-
-  @override
-  State<SearchScreen> createState() => _SearchScreenState();
-}
-
-class _SearchScreenState extends State<SearchScreen> {
-  bool isDark = false;
   @override
   Widget build(BuildContext context) {
-    final ThemeData themeData = ThemeData(
-        useMaterial3: true,
-        brightness: isDark ? Brightness.dark : Brightness.light);
     return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-            body: Column(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Search and Card List'),
+        ),
+        body: Column(
           children: <Widget>[
-            const Expanded(
-              child: SearchMusicBar(),
+            SearchBarWidget(),
+            Expanded(
+              child: CardListView(),
             ),
-            Expanded(child: ListView(children: const <Widget> [
-              Card(
-                child: ListTile(
-                  title: Text('Card 1'),
-                  subtitle: Text('Sub card 1'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  title: Text('Card 1'),
-                  subtitle: Text('Sub card 1'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  title: Text('Card 1'),
-                  subtitle: Text('Sub card 1'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  title: Text('Card 1'),
-                  subtitle: Text('Sub card 1'),
-                ),
-              ),
-
-            ],))
-            
           ],
-        )),
+        ),
       ),
-      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class SearchBarWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: const TextField(
+        decoration: InputDecoration(
+          hintText: 'Search...',
+          prefixIcon: Icon(Icons.search),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CardListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const <Widget>[
+        CardView(title: 'Card 1', subtitle: 'Subtitle 1'),
+        CardView(title: 'Card 2', subtitle: 'Subtitle 2'),
+        CardView(title: 'Card 2', subtitle: 'Subtitle 2'),
+        CardView(title: 'Card 2', subtitle: 'Subtitle 2'),
+        CardView(title: 'Card 2', subtitle: 'Subtitle 2'),
+        CardView(title: 'Card 2', subtitle: 'Subtitle 2'),
+        CardView(title: 'Card 2', subtitle: 'Subtitle 2'),
+
+        // ... Add more CardViews
+      ],
+    );
+  }
+}
+
+class CardView extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  const CardView({required this.title, required this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(subtitle),
+      ),
     );
   }
 }
